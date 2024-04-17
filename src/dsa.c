2429fceb9adf182cdeb5015bb4366ce41622eec1
Davide Bigotti <bigotti.davide@hotmail.com>
2024-04-17 21:53:39 +0200

    add insertion sort; integrate LSP

--- src/dsa.c
+++ src/dsa.c
@@ -1,3 +1,13 @@
-int algo() {
-	return 0;
+#include "sorting.h"
+
+int* insertion_sort(int* arr, int size) {
+	for(int i = 1; i < size; i++) {
+		int curr = arr[i];
+		int e = i - 1;
+		for(; e >= 0 && arr[e] > curr; e--) {
+			arr[e+1] = arr[e];
+		}
+		arr[e + 1] = curr;
+	}
+	return arr;
 }
--- tests/test_dsa.c
+++ tests/test_dsa.c
@@ -1,21 +1,30 @@
-#include "dsa.h"
+#include "sorting.h"
 #include "unity.h"
 #include <stdio.h>
+#include <string.h>
 
 void setUp() {
-	// called before each test
+        // called before each test
 }
 
 void tearDown() {
-	// called after each test
+        // called after each test
 }
 
-void test_example() {
-	TEST_ASSERT(algo() == 0);
+#define SIZE 5
+void test_insertion_sort() {
+        int input[SIZE] = {0, 1, 5, 2, 1};
+        int expected[SIZE] = {0, 1, 1, 2, 5};
+        int result[SIZE];
+
+        memcpy(result, input, SIZE*4);
+        insertion_sort(result, SIZE);
+
+        TEST_ASSERT_EQUAL_INT_ARRAY(expected, result, SIZE);
 }
 
 int main() {
-	UNITY_BEGIN();
-	RUN_TEST(test_example);
-	return UNITY_END();
+        UNITY_BEGIN();
+        RUN_TEST(test_insertion_sort);
+        return UNITY_END();
 }
